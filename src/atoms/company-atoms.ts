d9921d2afce07113900f9cc726ddaff9ad591456
joaoprocopio <joaovitorcprocopio@gmail.com>
2024-12-20 11:16:20 -0300

    fix

--- src/atoms/company-atoms.ts
+++ src/atoms/company-atoms.ts
@@ -1,16 +1,15 @@
 import { atomWithReset } from "jotai/utils"
 
-import { atomWithHash } from "~/atoms/utils/atom-with-hash"
 import type { TAsset, TCompany, TLocation } from "~/schemas/company-schemas"
 
 export const selectedCompanyAtom = atomWithReset<TCompany | undefined>(undefined)
 
 export const selectedAssetAtom = atomWithReset<TLocation | TAsset | undefined>(undefined)
 
-export const selectedCompanyIdAtom = atomWithHash("ci", "")
+export const selectedCompanyIdAtom = atomWithReset<string>("")
 
-export const selectedAssetNameAtom = atomWithHash("an", "")
+export const selectedAssetNameAtom = atomWithReset<string>("")
 
-export const selectedAssetStatusAtom = atomWithHash("as", "")
+export const selectedAssetStatusAtom = atomWithReset<string>("")
 
-export const selectedAssetIdAtom = atomWithHash("ai", "")
+export const selectedAssetIdAtom = atomWithReset<string>("")
