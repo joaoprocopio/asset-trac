e0405d6c42052e3f5df6e1b9a6d6c7fe373d1858
joaoprocopio <joaovitorcprocopio@gmail.com>
2024-12-20 10:48:08 -0300

    vai

--- src/entry.client.tsx
+++ src/entry.client.tsx
@@ -1,12 +1,17 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import { startTransition, StrictMode } from "react"
 import { hydrateRoot } from "react-dom/client"
 import { HydratedRouter } from "react-router/dom"
+
+const queryClient = new QueryClient()
 
 startTransition(() => {
   hydrateRoot(
     document,
     <StrictMode>
-      <HydratedRouter />
+      <QueryClientProvider client={queryClient}>
+        <HydratedRouter />
+      </QueryClientProvider>
     </StrictMode>
   )
 })
