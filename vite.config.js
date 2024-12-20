fa273885a85893ef34d36c1123c0485a76547a74
joaoprocopio <joaovitorcprocopio@gmail.com>
2024-12-20 10:29:26 -0300

    vai

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
-import react from "@vitejs/plugin-react"
+import { reactRouter } from "@react-router/dev/vite"
 import { fileURLToPath, URL } from "url"
 import { defineConfig } from "vite"
 import svgr from "vite-plugin-svgr"
 
 export default defineConfig(() => {
   return {
-    plugins: [react(), svgr()],
+    plugins: [reactRouter(), svgr()],
     server: {
       port: 3000,
     },
