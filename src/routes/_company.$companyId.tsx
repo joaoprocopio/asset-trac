d7ed940416ef8e7f176ed0a645269a61b93263c7
joaoprocopio <joaovitorcprocopio@gmail.com>
2024-12-22 14:39:45 -0300

    junta as parada

--- src/routes/_company.$companyId.tsx
+++ src/routes/_company.$companyId.tsx
@@ -3,13 +3,11 @@ import { useAtom } from "jotai"
 import { RESET } from "jotai/utils"
 import { useParams } from "react-router"
 
-import { Card, CardContent } from "~/components/card"
+import { Card, CardContent, CardHeader, CardTitle } from "~/components/card"
 import { CompanyAssetsDetails } from "~/components/company-assets/company-assets-details"
-import { CompanyAssetsDetailsSkeleton } from "~/components/company-assets/company-assets-details-skeleton"
 import { CompanyAssetsFilter } from "~/components/company-assets/company-assets-filter"
-import { CompanyAssetsHeader } from "~/components/company-assets/company-assets-header"
 import { CompanyAssetsTree } from "~/components/company-assets/company-assets-tree"
-import { CompanyAssetsTreeSkeleton } from "~/components/company-assets/company-assets-tree-skeleton"
+import { Typography } from "~/components/typography"
 import { queryClient } from "~/lib/query/query-client"
 import { assetsOptions, locationsOptions, selectedCompanyOptions } from "~/lib/query/query-options"
 import { selectedAssetNameAtom, selectedAssetStatusAtom } from "~/stores/company-store"
@@ -34,6 +32,7 @@ export default function CompanyAssetsPage() {
 
   const locations = useQuery(locationsOptions(params.companyId!))
   const assets = useQuery(assetsOptions(params.companyId!))
+  const selectedCompany = useQuery(selectedCompanyOptions(params.companyId!))
 
   const handleChangeSelectedAssetName = (nextAssetQuery: string | typeof RESET) => {
     setSelectedAssetName(nextAssetQuery)
@@ -45,7 +44,18 @@ export default function CompanyAssetsPage() {
 
   return (
     <Card className="flex h-full flex-col">
-      <CompanyAssetsHeader className="border-b px-6 py-4" />
+      <CardHeader className="border-b px-6 py-4">
+        <CardTitle className="inline-flex items-end gap-1">
+          <span>Assets</span>
+          <span>
+            {selectedCompany.isSuccess && (
+              <Typography className="font-normal" affects="muted">
+                / {selectedCompany.data?.name} Unit
+              </Typography>
+            )}
+          </span>
+        </CardTitle>
+      </CardHeader>
 
       <CardContent className="grid flex-grow grid-cols-2 overflow-hidden p-0">
         <div className="grid grid-rows-[4rem_1fr] border-r">
@@ -57,19 +67,12 @@ export default function CompanyAssetsPage() {
             handleChangeSelectedAssetStatus={handleChangeSelectedAssetStatus}
           />
 
-          {locations.isSuccess && assets.isSuccess ? (
+          {locations.isSuccess && assets.isSuccess && (
             <CompanyAssetsTree className="pl-6" locations={locations.data} assets={assets.data} />
-          ) : (
-            <CompanyAssetsTreeSkeleton />
           )}
         </div>
 
-        {locations.isSuccess && assets.isSuccess ? (
-          // TODO: passar o asset
-          <CompanyAssetsDetails />
-        ) : (
-          <CompanyAssetsDetailsSkeleton />
-        )}
+        {locations.isSuccess && assets.isSuccess && <CompanyAssetsDetails />}
       </CardContent>
     </Card>
   )
