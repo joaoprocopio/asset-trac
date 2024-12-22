a8b8993f550aab2577f11937d68a34b4e58eef1d
joaoprocopio <joaovitorcprocopio@gmail.com>
2024-12-21 22:08:54 -0300

    sem spread

--- src/components/company-assets/company-assets-header.tsx
+++ src/components/company-assets/company-assets-header.tsx
@@ -5,13 +5,8 @@ import { CardHeader, CardTitle } from "~/components/card"
 import { Skeleton } from "~/components/skeleton"
 import { Typography } from "~/components/typography"
 import { selectedCompanyOptions } from "~/lib/query/query-options"
-import type { TCompany } from "~/schemas/company-schemas"
 
-export interface ICompanyAssetsHeaderProps extends React.HTMLAttributes<HTMLDivElement> {
-  selectedCompany?: TCompany
-}
-
-export function CompanyAssetsHeader({ ...props }: ICompanyAssetsHeaderProps) {
+export function CompanyAssetsHeader(props: React.HTMLAttributes<HTMLDivElement>) {
   const params = useParams()
   const loaderData = useLoaderData()
   const queryClient = useQueryClient()
