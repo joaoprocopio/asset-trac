577ff354d321da9098edb1f7f76e56b591d8a3f3
joaoprocopio <joaovitorcprocopio@gmail.com>
2024-12-22 18:20:27 -0300

    util pro assets graph

--- src/components/company-assets/company-assets-tree.tsx
+++ src/components/company-assets/company-assets-tree.tsx
@@ -1,26 +1,17 @@
 import { useQuery } from "@tanstack/react-query"
-import { useMemo } from "react"
 import { useParams } from "react-router"
 
 import { AssetIdKey, AssetNameKey, AssetStatusKey } from "~/constants/company-constants"
 import { useSearchParam } from "~/hooks/use-search-param"
-import { Graph } from "~/lib/graph"
-import { assetsOptions, locationsOptions } from "~/lib/query/query-options"
-import type { TAsset, TAssets, TLocation, TLocations } from "~/schemas/company-schemas"
+import { assetsGraphOptions, assetsOptions, locationsOptions } from "~/lib/query/query-options"
 
 export function CompanyAssetsTree(props: React.HTMLAttributes<HTMLDivElement>) {
   const params = useParams()
 
   const locations = useQuery(locationsOptions(params.companyId!))
   const assets = useQuery(assetsOptions(params.companyId!))
-
-  const graph = useQuery({
-    queryFn: async () => {
-      const nextGraph = buildGraph(locations.data!, assets.data!)
-
-      return nextGraph
-    },
-    queryKey: ["graph", params.companyId],
+  const assetsGraph = useQuery({
+    ...assetsGraphOptions(params.companyId!, locations.data!, assets.data!),
     enabled: locations.isSuccess && assets.isSuccess,
   })
 
@@ -29,51 +20,4 @@ export function CompanyAssetsTree(props: React.HTMLAttributes<HTMLDivElement>) {
   const [selectedAssetId, setSelectedAssetId] = useSearchParam({ paramKey: AssetIdKey })
 
   return <div {...props}>arvore</div>
-}
-
-function buildGraph(locations: TLocations, assets: TAssets) {
-  type TLocationNode = TLocation & { type: "location" }
-  type TAssetNode = Omit<TAsset & { type: "component" | "asset" }, "locationId">
-
-  const graph = new Graph<TLocationNode | TAssetNode>()
-
-  for (const location of locations) {
-    graph.addNode(location.id, {
-      type: "location",
-      id: location.id,
-      name: location.name.toLowerCase(),
-      parentId: location.parentId,
-    })
-
-    if (location.parentId) {
-      if (!graph.hasNode(location.parentId)) {
-        graph.addNode(location.parentId)
-      }
-
-      graph.addEdge(location.parentId, location.id)
-    }
-  }
-
-  for (const asset of assets) {
-    graph.addNode(asset.id, {
-      type: asset.sensorId ? "component" : "asset",
-      id: asset.id,
-      name: asset.name.toLowerCase(),
-      parentId: asset.locationId || asset.parentId,
-      sensorType: asset.sensorType,
-      status: asset.status,
-      gatewayId: asset.gatewayId,
-      sensorId: asset.sensorId,
-    })
-
-    if (asset.parentId) {
-      if (!graph.hasNode(asset.parentId)) {
-        graph.addNode(asset.parentId)
-      }
-
-      graph.addEdge(asset.parentId, asset.id)
-    }
-  }
-
-  return graph
 }
--- src/lib/query/query-options.ts
+++ src/lib/query/query-options.ts
@@ -1,5 +1,7 @@
 import { queryOptions } from "@tanstack/react-query"
 
+import { Graph } from "~/lib/graph"
+import type { TAsset, TAssets, TLocation, TLocations } from "~/schemas/company-schemas"
 import { CompanyServices } from "~/services/company-services"
 
 export const companiesOptions = () =>
@@ -25,4 +27,55 @@ export const assetsOptions = (companyId: string) =>
   queryOptions({
     queryFn: (args) => CompanyServices.getCompanyAssets(companyId, args.signal),
     queryKey: ["company-assets", companyId],
+  })
+
+export const assetsGraphOptions = (companyId: string, locations: TLocations, assets: TAssets) =>
+  queryOptions({
+    queryFn: async () => {
+      type TLocationNode = TLocation & { type: "location" }
+      type TAssetNode = Omit<TAsset & { type: "component" | "asset" }, "locationId">
+
+      const graph = new Graph<TLocationNode | TAssetNode>()
+
+      for (const location of locations) {
+        graph.addNode(location.id, {
+          type: "location",
+          id: location.id,
+          name: location.name.toLowerCase(),
+          parentId: location.parentId,
+        })
+
+        if (location.parentId) {
+          if (!graph.hasNode(location.parentId)) {
+            graph.addNode(location.parentId)
+          }
+
+          graph.addEdge(location.parentId, location.id)
+        }
+      }
+
+      for (const asset of assets) {
+        graph.addNode(asset.id, {
+          type: asset.sensorId ? "component" : "asset",
+          id: asset.id,
+          name: asset.name.toLowerCase(),
+          parentId: asset.locationId || asset.parentId,
+          sensorType: asset.sensorType,
+          status: asset.status,
+          gatewayId: asset.gatewayId,
+          sensorId: asset.sensorId,
+        })
+
+        if (asset.parentId) {
+          if (!graph.hasNode(asset.parentId)) {
+            graph.addNode(asset.parentId)
+          }
+
+          graph.addEdge(asset.parentId, asset.id)
+        }
+      }
+
+      return graph
+    },
+    queryKey: ["graph", companyId],
   })
