ab00333f50513e84ff7ab2df052fd1e02661c5f4
joaoprocopio <joaovitorcprocopio@gmail.com>
2024-12-22 13:54:39 -0300

    rm ports

--- vite.config.js
+++ vite.config.js
@@ -6,12 +6,6 @@ import svgr from "vite-plugin-svgr"
 export default defineConfig(() => {
   return {
     plugins: [reactRouter(), svgr()],
-    server: {
-      port: 3000,
-    },
-    preview: {
-      port: 3000,
-    },
     resolve: {
       alias: {
         "~": fileURLToPath(new URL("./src", import.meta.url)),
