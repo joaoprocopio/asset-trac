ddaaafecb0006192fe28656f97d0cf9a2387b6ce
joaoprocopio <joaovitorcprocopio@gmail.com>
2024-12-25 23:58:38 -0300

    t

--- src/routes/_company.$companyId._asset._index.tsx
+++ src/routes/_company.$companyId._asset._index.tsx
@@ -7,7 +7,7 @@ export default function AssetEmpty() {
     <div className="row-span-2 space-y-1.5 self-center text-center">
       <InboxIcon className="h-14 w-full" />
 
-      <Typography variant="h3">Empty</Typography>
+      <Typography variant="h3">No asset selected</Typography>
       <Typography className="mx-auto" affects="muted">
         Select any location, asset or component
       </Typography>
