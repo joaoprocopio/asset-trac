69004444fc8b1349c02a429f76afaa05b2b889bb
joaoprocopio <joaovitorcprocopio@gmail.com>
2024-12-28 21:48:16 -0300

    vai

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,3 +1,11 @@
 export function array(length: number) {
   return Array.from({ length: length })
 }
+
+export function isNil(value: unknown) {
+  return value == null
+}
+
+export function isNumber(value: unknown) {
+  return Number.isFinite(parseInt(value as string))
+}
