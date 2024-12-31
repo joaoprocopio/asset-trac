0b7db300280fc1e3726b1e6f60026b0e64c7b7ac
joaoprocopio <joaovitorcprocopio@gmail.com>
2024-12-31 01:03:06 -0300

    more readable

--- src/lib/tree/tree.ts
+++ src/lib/tree/tree.ts
@@ -11,27 +11,39 @@ export function buildFlatTree<Node>(graph: Graph<Node>): TFlatTree<Node> {
   const visited = new Set<TGraphNodeId>()
 
   function traverse(nodeId: TGraphNodeId, level: number = 0): void {
+    if (visited.has(nodeId)) {
+      return undefined
+    }
+
     visited.add(nodeId)
 
     const node = graph.getNode(nodeId)! as TFlatTreeNode<Node>
     node.level = level
     flatTree.push(node)
 
-    if (graph.hasEdge(nodeId)) {
-      const edge = graph.getEdge(nodeId)!
+    if (!graph.hasEdge(nodeId)) {
+      return undefined
+    }
 
-      for (const edgeNodeId of edge.values()) {
-        const edgeNode = graph.getNode(edgeNodeId)
+    const edge = graph.getEdge(nodeId)!
 
-        if (!visited.has(edgeNodeId) && edgeNode?.parentId === nodeId) {
-          traverse(edgeNodeId, level + 1)
-        }
+    for (const edgeNodeId of edge.values()) {
+      const edgeNode = graph.getNode(edgeNodeId)
+
+      if (visited.has(edgeNodeId) && edgeNode?.parentId !== nodeId) {
+        continue
       }
+
+      traverse(edgeNodeId, level + 1)
     }
   }
 
-  for (const root of graph.getAllRoots()) {
-    traverse(root)
+  for (const rootNodeId of graph.getAllRoots()) {
+    if (visited.has(rootNodeId)) {
+      continue
+    }
+
+    traverse(rootNodeId)
   }
 
   return flatTree
